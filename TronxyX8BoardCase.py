987f35e0c0c3dba0887b9f40127c2684147a3aa4
Giuseppe Scarlato <giuseppe499@live.it>
2021-11-20 16:42:06 +0100

    Aggiunta Licenza

--- TronxyX8BoardCase.py
+++ TronxyX8BoardCase.py
@@ -1,3 +1,21 @@
+'''
+Procedural motherboard/electronics case for cxy-v2-0508 (Tronxy X8 board)
+Copyright (C) 2021  Giuseppe Scarlato
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 import cadquery as cq
 from cadquery import exporters
 
